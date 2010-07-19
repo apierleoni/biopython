02105aad097d8c1d447601bec6d3b29b34da7bf2
Andrea Pierleoni <andreapierleoni@macbook-pro-di-andrea-pierleoni.local>
2010-07-19 15:53:03 +0200

    initialize module

---
+++ BioSQL2py/__init__.py
@@ -0,0 +1,9 @@
+'''Code for storing and retrieving biological sequences from a BioSQL
+relational database.
+
+this version uses web2py DAL to comunicate with DB backends.
+Additional features added for simplier querying and handling bioentry relationships
+
+See:
+http://www.biosql.org/'''
+
